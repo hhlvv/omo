61ac05580756558919d1513127ef7a3c14c6afe5
fari <nohh27@naver.com>
2020-01-09 00:39:25 +0900

    * vue example test

--- Vue/main.js
+++ Vue/main.js
@@ -2,6 +2,29 @@ var app = new Vue({
     el : "#app",
     data : {
         product : 'Socks',
-        image : './flower.jpg'
+        image : './img/flower.jpg',
+        inStock : true,
+        details : ["80% cotton", "20% polyster", "Gender-neutral"],
+        variants : [
+            {
+                variantId:2234,
+                variantColor : "green",
+                variantImage : "./img/green.png"
+            },
+            {
+                variantId : 2235,
+                variantColor : "blue",
+                variantImage : "./img/blue.png"
+            }
+        ],
+        cart : 0
+    },
+    methods : {
+        addToCart(){
+            this.cart += 1
+        },
+        updateProduct(variantImage){
+            this.image = variantImage
+        }
     }
 });
