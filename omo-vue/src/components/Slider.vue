6711065e38baae405e8885764867825bbaa8a961
fari <nohh27@naver.com>
2020-03-19 20:41:47 +0900

    * carousel3d add
    Carousel 3d into component using import _____> 빌드O , 작동 오류

--- omo-vue/src/components/Slider.vue
+++ omo-vue/src/components/Slider.vue
@@ -1,59 +1,11 @@
 <template>
     <div id="slider">
-        <v-app id="slider_div">
-            <v-carousel
-            cycle
-            height="400"
-            hide-delimiter-background
-            show-arrows-on-hover
-            >
-            <v-carousel-item
-                v-for="(slide, i) in slides"
-                :key="i"
-            >
-                <v-sheet
-                :color="colors[i]"
-                height="100%"
-                >
-                <v-row
-                    class="fill-height"
-                    align="center"
-                    justify="center"
-                >
-                    <div class="display-3">{{ slide }} Slide</div>
-                </v-row>
-                </v-sheet>
-            </v-carousel-item>
-            </v-carousel>
-        </v-app>
+        <h1>test...</h1>
     </div>
 </template>
 
 <script>
   export default {
-    name: 'slider-component',
-    data () {
-        return {
-            colors: [
-                'indigo',
-                'warning',
-                'pink darken-2',
-                'red lighten-1',
-                'deep-purple accent-4',
-            ],
-            slides: [
-                'First',
-                'Second',
-                'Third',
-                'Fourth',
-                'Fifth',
-            ],
-            }
-    },
-    methods: {
-      showTitle () {
-        this.title = 'slider Component'
-      }
-    }
+    
   }
 </script>
